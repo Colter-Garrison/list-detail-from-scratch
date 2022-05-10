c5633b143061d8fe2ce031703fdaad4c8b1d3615
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-10 16:26:56 -0700

    test passing

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderMollusks } from '../render-utils.js';
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -15,4 +17,26 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('Pacific Banana Slug Test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="mollusk"><a href="./mollusk-page/?id=1"><h1>Pacific Banana Slug</h1><img src="./assets/banana-slug.png"></a></div>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const pacificBananaSlug = {
+        id: '1',
+        name: 'Pacific Banana Slug',
+        image: './assets/banana-slug.png',
+        native: 'native to Oregon',
+        length: 'can grow up to 25cm long',
+        found: ['Alaska', ' Oregon', ' and California'],
+    };
+
+    const actual = renderMollusks(pacificBananaSlug);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
 });
