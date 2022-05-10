74d1571ba90afc4541b8b3ce1d15f62630964e7c
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-10 15:01:53 -0700

    everything working. Time to css

--- mollusk-page/mollusk-page.js
+++ mollusk-page/mollusk-page.js
@@ -1,26 +1,24 @@
 import { findById } from '../utils.js';
 import { mollusks } from '../data.js';
-console.log('hello from mollusks page');
+// console.log('hello from mollusks page');
 
 const params = new URLSearchParams(window.location.search);
-console.log(params.get('id'));
+// console.log(params.get('id'));
 
 const mollusk = findById(params.get('id'), mollusks);
-console.log(mollusk);
+// console.log(mollusk);
 
 const molluskName = document.getElementById('mollusk-name');
 molluskName.textContent = mollusk.name;
 
-const molluskImage = document.createElement('img');
+const molluskImage = document.getElementById('mollusk-image');
 molluskImage.src = `.${mollusk.image}`;
 
-const molluskNative = document.createElement('p');
-molluskNative.textContent = `This mollusk is ${mollusk.native}!`;
+const molluskNative = document.getElementById('mollusk-native');
+molluskNative.textContent = mollusk.native;
 
-const molluskLength = document.createElement('p');
-molluskLength.textContent = `This mollusk ${mollusk.length}!`;
+const molluskLength = document.getElementById('mollusk-length');
+molluskLength.textContent = mollusk.length;
 
-const molluskFound = document.createElement('p');
-molluskFound.textContent = `Among other places this mollusk can commonly be found in ${mollusk.found}.`;
-
-molluskName.append(molluskImage, molluskNative, molluskLength, molluskFound);
+const molluskFound = document.getElementById('mollusk-found');
+molluskFound.textContent = mollusk.found;
